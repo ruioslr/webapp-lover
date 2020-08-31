35b9b215bd90b61aa3bac84e35153e0ae5cb9a75
ruios <ruioslr@163.com>
2020-08-31 10:31:24 +0800

    feat: 添加单元测试

---
+++ src/__tests__/home.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Home from "../pages/home";
+
+let container: Element = null;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+describe('home页面', () => {
+    it("是否渲染", () => {
+        act(() => {
+            render(<Home />, container);
+        });
+        expect(container.querySelector('div')).not.toBeNull();
+    });
+})
+
