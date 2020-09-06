a08038943d2bd0ffafa593b0c9fbb0cba9c272c9
ruios <ruioslr@163.com>
2020-09-06 19:04:23 +0800

    fix: 天气的bug

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -2,7 +2,12 @@ import React, { useEffect } from 'react';
 
 export default function Weather() {
   useEffect(() => {
+    const scriptDom = document.querySelector('#weatherScript');
+    if (scriptDom) {
+      scriptDom.remove();
+    }
     const script = document.createElement('script');
+    script.id = 'weatherScript';
     script.src =
       'https://widget.heweather.net/standard/static/js/he-standard-common.js?v=1.1';
     document.body.appendChild(script);
